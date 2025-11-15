ba4b1177d02281b4e29561c8a66538471c173b60
keshanta <keshanta.smith@ufl.edu>
2025-11-14 20:44:38 -0500

    Partner 1 functions

--- calculator.py
+++ calculator.py
@@ -4,8 +4,32 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+#Partner 1: Keshanta Smith
+#Partner 2: Angel Moreno
+
+import math
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a * b
+
+def fiv(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def log(a, b):
+    if a <= 0 or b <= 0:
+        raise ValueError("Invalid arguments for logarithm")
+    return math.log(b, a)
+def exp(a, b):
+    return a ** b
 
 
