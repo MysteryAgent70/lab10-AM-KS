6fdf477be594e3ca8ead1be0e419542b437bcd1f
keshanta <keshanta.smith@ufl.edu>
2025-11-14 21:17:56 -0500

    fixed Partener 1 functions

--- calculator.py
+++ calculator.py
@@ -12,25 +12,23 @@ import math
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
-    return a * b
-
-def fiv(a, b):
+def divide(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return b / a
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or b <= 0:
         raise ValueError("Invalid arguments for logarithm")
     return math.log(b, a)
-def exp(a, b):
+
+def exponent(a, b):
     return a ** b
 =======
 # First example
